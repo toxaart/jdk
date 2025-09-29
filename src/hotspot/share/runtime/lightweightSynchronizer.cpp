b51673a6e49ceca9667d690b0c9bc7755d82c698
Anton Artemov <anton.artemov@oracle.com>
2025-09-29 11:27:31 +0200

    Rollback

--- src/hotspot/share/runtime/lightweightSynchronizer.cpp
+++ src/hotspot/share/runtime/lightweightSynchronizer.cpp
@@ -1027,7 +1027,7 @@ ObjectMonitor* LightweightSynchronizer::inflate_and_enter(oop object, BasicLock*
     monitor = get_or_insert_monitor(object, current, cause);
   }
 
-  if (false && monitor->try_enter(locking_thread)) {
+  if (monitor->try_enter(locking_thread)) {
     return monitor;
   }
 
