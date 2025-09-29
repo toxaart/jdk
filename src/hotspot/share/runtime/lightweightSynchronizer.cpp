0b50549c4e9c79a8755e95e2b8f688f93d9e4be4
Anton Artemov <anton.artemov@oracle.com>
2025-09-29 11:59:05 +0200

    Rollback

--- src/hotspot/share/runtime/lightweightSynchronizer.cpp
+++ src/hotspot/share/runtime/lightweightSynchronizer.cpp
@@ -1128,7 +1128,7 @@ ObjectMonitor* LightweightSynchronizer::inflate_and_enter(oop object, BasicLock*
 
   if (current == locking_thread) {
     // One round of spinning
-    if (monitor->contentions() < 3 && monitor->spin_enter(locking_thread)) {
+    if (monitor->spin_enter(locking_thread)) {
       return monitor;
     }
 
