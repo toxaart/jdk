b06e98e4f7454c3d203e7a8eacd52dd2475c3b6a
Anton Artemov <anton.artemov@oracle.com>
2025-10-08 10:46:11 +0200

    Cache 512

--- src/hotspot/share/runtime/lockStack.hpp
+++ src/hotspot/share/runtime/lockStack.hpp
@@ -132,7 +132,7 @@ class LockStack {
 class OMCache {
   friend class VMStructs;
  public:
-  static constexpr int CAPACITY = 64;
+  static constexpr int CAPACITY = 512;
 
  private:
   struct OMCacheEntry {
